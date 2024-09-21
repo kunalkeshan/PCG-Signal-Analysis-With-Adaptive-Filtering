cd1cfb7e67de89996b536233f42b833e6b22097b
kunalkeshan <kunalkeshan12@gmail.com>
2024-09-21 23:13:09 +0530

    chore: added layout sections for project page

--- client/app/project/page.tsx
+++ client/app/project/page.tsx
@@ -1,9 +1,15 @@
 import Hero from '@/components/project/hero';
+import TechnicalDetails from '@/components/project/technical-details';
+import CodeSnippets from '@/components/project/code-snippets';
+import ResultsAndAnalysis from '@/components/project/results-and-analysis';
 
 export default function Project() {
 	return (
 		<main>
 			<Hero />
+			<TechnicalDetails />
+			<CodeSnippets />
+			<ResultsAndAnalysis />
 		</main>
 	);
 }
