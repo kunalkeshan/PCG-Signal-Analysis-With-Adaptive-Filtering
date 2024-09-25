8abd3f4ba33d4ad82a0bbe636cfb387a7ab7bf13
kunalkeshan <kunalkeshan12@gmail.com>
2024-09-25 14:27:54 +0530

    chore: update contact page
    - closes #21

--- client/app/contact/page.tsx
+++ client/app/contact/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/contact/hero';
+import TeamMembers from '@/components/common/team-members';
 
 export default function Contact() {
 	return (
 		<main>
 			<Hero />
+			<TeamMembers />
 		</main>
 	);
 }
