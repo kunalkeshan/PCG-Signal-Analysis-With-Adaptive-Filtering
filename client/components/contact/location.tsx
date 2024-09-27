02bd3beb16ea2ee79ed74c1775794ce24c487b07
Kunal Keshan <kunalkeshan12@gmail.com>
2024-09-27 21:36:39 +0530

    chore: contact locations section (wip)

--- client/components/contact/location.tsx
+++ client/components/contact/location.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 const Location = () => {
 	return (
-		<section className='w-full max-w-screen-xl mx-auto p-6 md:p-16'>
-			<h2 className='font-heading font-semibold text-3xl text-center'>
-				Location
-			</h2>
+		<section className='w-full p-6 md:p-16'>
+			<div className='max-w-screen-xl mx-auto bg-background grid grid-cols-1 lg:grid-cols-3 gap-5'>
+				<div>
+					<h2 className='font-heading font-semibold text-3xl text-center'>
+						Location
+					</h2>
+				</div>
+				<div className='col-span-2'>{/* Location iframe */}</div>
+			</div>
 		</section>
 	);
 };
