a6fe34ff5e43e93a7a4c00be2fe25a9f8d936bdd
kunalkeshan <kunalkeshan12@gmail.com>
2024-10-04 14:33:04 +0530

    chore: remove background from contact location

--- client/components/contact/location.tsx
+++ client/components/contact/location.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Location = () => {
 	return (
 		<section className='w-full p-6 md:p-16'>
-			<div className='max-w-screen-xl mx-auto bg-background grid grid-cols-1 lg:grid-cols-3 gap-5'>
+			<div className='max-w-screen-xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5'>
 				<div>
 					<h2 className='font-heading font-semibold text-3xl text-center'>
 						Location
